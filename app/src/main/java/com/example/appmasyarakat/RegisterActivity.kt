92f373c95782576ac9511858d906efb53fb6838a
shenialusi <shenia.lusi2004@gmail.com>
2023-11-29 09:41:15 +0700

    loginactivity dan registeractivity

--- app/src/main/java/com/example/appmasyarakat/RegisterActivity.kt
+++ app/src/main/java/com/example/appmasyarakat/RegisterActivity.kt
@@ -2,10 +2,33 @@ package com.example.appmasyarakat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Scanner
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+    fun main() {
+        val scanner = Scanner(System.`in`)
+
+        var username: String
+        var password: String
+        var confirmPassword: String
+
+        print("Masukkan username: ")
+        username = scanner.nextLine()
+
+        print("Masukkan password: ")
+        password = scanner.nextLine()
+
+        print("Masukkan kembali password: ")
+        confirmPassword = scanner.nextLine()
+
+        if (password == confirmPassword) {
+            println("Registrasi berhasil! Username: $username, Password: $password")
+        } else {
+            println("Registrasi gagal. Password tidak cocok.")
+        }
     }
 }
