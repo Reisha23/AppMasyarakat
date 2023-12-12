dcadfc20431db2bc4b88e5e9fad1bb0da9763317
Reisha23 <esareisha01@gmail.com>
2023-12-12 10:19:29 +0700

    firebase login and register

--- app/src/main/java/com/example/appmasyarakat/MainActivity.kt
+++ app/src/main/java/com/example/appmasyarakat/MainActivity.kt
@@ -11,45 +11,37 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     private lateinit var btnLogin: ImageView
     private lateinit var binding:ActivityMainBinding
-    private lateinit var firebaseAuth: Firebase
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var btnSignup: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = firebaseAuth.getInstance()
-        binding.email.setOnClickListener{
-            val intent = Intent(this , RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.btnLogin.setOnClickListener{
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnLogin.setOnClickListener {
             val email = binding.email.text.toString()
             val pass = binding.password.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
-                    firebaseAuth.mainwithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
-                        }
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, Home::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
+                }
 
             } else {
                 Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
             }
         }
 
-        setContentView(R.layout.activity_main)
-        btnLogin = findViewById(R.id.btnLogin)
-        
-        btnLogin.setOnClickListener{
-            val mvHome = Intent(this, Home::class.java)
-            startActivity(mvHome)
+        binding.btnSignUp.setOnClickListener {
+            val mvRegisterActivity = Intent(this,RegisterActivity::class.java)
+            startActivity(mvRegisterActivity)
         }
     }
+}
--- app/src/main/java/com/example/appmasyarakat/RegisterActivity.kt
+++ app/src/main/java/com/example/appmasyarakat/RegisterActivity.kt
@@ -1,34 +1,42 @@
 package com.example.appmasyarakat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.appmasyarakat.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 import java.util.Scanner
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-    }
-    fun main() {
-        val scanner = Scanner(System.`in`)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var username: String
-        var password: String
-        var confirmPassword: String
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener{
+            val email = binding.rgstEmail.text.toString()
+            val pass = binding.rgstPass.text.toString()
+            val rePass = binding.rgstRePass.text.toString()
 
-        print("Masukkan username: ")
-        username = scanner.nextLine()
-
-        print("Masukkan password: ")
-        password = scanner.nextLine()
-
-        print("Masukkan kembali password: ")
-        confirmPassword = scanner.nextLine()
-
-        if (password == confirmPassword) {
-            println("Registrasi berhasil! Username: $username, Password: $password")
-        } else {
-            println("Registrasi gagal. Password tidak cocok.")
+            if (email.isNotEmpty() && pass.isNotEmpty() && rePass.isNotEmpty()) {
+                if (pass != rePass){
+                    Toast.makeText(this, "Masukkan Password Yang Sama", Toast.LENGTH_SHORT).show()
+                }else{
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
         }
     }
 }
