6dc76789b70a5b3802e94b7dd9cf6dbc926aaeea
shenialusi <shenia.lusi2004@gmail.com>
2023-12-15 18:20:01 +0700

    Menambahkan BeritaModel.kt dengan membuat data kelas untuk menyimpan data properti seperti nama, alamat, foto, email dan informasi berita

---
+++ app/src/main/java/com/example/appmasyarakat/BeritaModel.kt
@@ -0,0 +1,8 @@
+package com.example.appmasyarakat
+
+data class BeritaModel (
+    val nama: String = "test",
+    val alamat: String = "test",
+    val foto: String = "test",
+    val informasi: String = "Informasi",
+val email: String = "Email")
