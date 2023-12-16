accbbbeaebceb7edd423a3c660dd77530ecf52d3
Reisha23 <esareisha01@gmail.com>
2023-12-16 19:36:08 +0700

    perbaikan adapter,dan BeritaViewModel

---
+++ app/src/main/java/com/example/appmasyarakat/BeritaModel.kt
@@ -0,0 +1,8 @@
+package com.example.appmasyarakat
+
+data class BeritaModel(
+    val nama: String = "test",
+    val alamat: String = "Test",
+    val foto: String = "test",
+    val informasi: String = "informasi",
+val email : String ="email")
---
+++ app/src/main/java/com/example/appmasyarakat/BeritaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.appmasyarakat
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BeritaViewModel : ViewModel() {
+    var _listberita: MutableLiveData<MutableList<BeritaModel>> = MutableLiveData(listBerita)
+
+    val lisBerita: LiveData<MutableList<BeritaModel>>
+        get() = _listberita
+
+    var _listBeritaHistory: MutableLiveData<MutableList<BeritaModel>> = MutableLiveData()
+
+    val listBeritaHistory: LiveData<MutableList<BeritaModel>>
+        get() = _listBeritaHistory
+}
